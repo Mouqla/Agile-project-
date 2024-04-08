25cb86a4bd97a461abfcc9d1d2ef27276819f69d
bllnr <nordmarkisabell@gmail.com>
2024-04-08 10:36:53 +0200

    Add click to show latitude/longtitude

--- static/settings.js
+++ static/settings.js
@@ -3,3 +3,16 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+
+// Popup when clicking on map, displays latitude & longitude of click location
+function onMapClick(e) {
+    var popup = L.popup()
+    .setLatLng(e.latlng)
+    .setContent("You clicked at: " + e.latlng)
+    .openOn(map);
+}
+
+map.on('click', onMapClick);
+
+
