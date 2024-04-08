db5a1a6ed53e1251dbd2d622d31aa304a324447a
bllnr <52850477+bllnr@users.noreply.github.com>
2024-04-08 18:01:54 +0200

    Parse data from API, display pollution in popup

--- static/settings.js
+++ static/settings.js
@@ -4,20 +4,34 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-// Popup when clicking on map, displays latitude & longitude of click location
+// Popup when clicking on map, displays latitude & longitude as well as closest measurement
 async function onMapClick(e) {
     const lat = e.latlng["lat"];
     const lng = e.latlng["lng"];
+    const radius = 5000;
 
-    const response = await fetch(`/api/get_air?lat=${lat.toFixed(3)}&lng=${lng.toFixed(3)}&radius=1000`);
+    const response = await fetch(`/api/get_air?lat=${lat.toFixed(3)}&lng=${lng.toFixed(3)}&radius=${radius}`);
 
     const retJson = await response.json(); 
 
-    console.log(retJson);
-    
+    //console.log(retJson);
+
+    var displayStr = ""
+    var apiResult = Object.values(retJson)[1][0]
+    try {
+        var closestLat = apiResult.coordinates.latitude;
+        var closestLong = apiResult.coordinates.longitude;
+        displayStr = `Air Pollution at closest station (${closestLat}, ${closestLong}) is `     
+        for(let i = 0; i < apiResult.measurements.length; i++){
+            displayStr += apiResult.measurements[i].parameter + ": ";
+            displayStr += apiResult.measurements[i].value + " ";
+            displayStr += apiResult.measurements[i].unit;
+            displayStr += "\n";}
+    } catch (error) {displayStr = `No results within ${radius} meters`;}
+
     var popup = L.popup()
         .setLatLng(e.latlng)
-        .setContent("You clicked at: " + retJson)
+        .setContent(displayStr)
         .openOn(map);
 }
 
