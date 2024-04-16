e91bcf9b429f774a182af483fe5ff1f0aa4a035b
Elias Nilsson <elinil0221@gmail.com>
2024-04-16 14:14:37 +0200

    add Heatmap

--- static/settings.js
+++ static/settings.js
@@ -103,3 +103,49 @@ function createAndAppendFrame(content) {
 
 map.on('click', onMapClick);
 
+async function fetchAirQualityData() {
+    const dataSet = []
+    for (let i = 1; i< 19; i++){
+        if (i == 17){
+            continue
+        }else{
+            const url = /api/get_points?page=${i}
+            const response = await fetch(url);
+            const data = await response.json();
+            dataSet.push(...data.results)
+            console.log(data)
+        }
+    }
+    console.log(dataSet)
+    return dataSet;
+}
+
+async function addHeatMap(){
+    const data = await fetchAirQualityData();
+    const heatPoints = [];
+
+    data.forEach(item => {
+        if (item.coordinates && item.measurements){
+            const value = item.measurements[0].value;
+            heatPoints.push([
+                item.coordinates.latitude,
+                item.coordinates.longitude,
+                value
+            ]);
+        }
+    });
+    console.log(heatPoints)
+    L.heatLayer(heatPoints, {max:3, blur:0, radius: 15, gradient: {
+        '0': 'Navy', '0.25': 'Navy',
+        '0.26': 'Green',
+        '0.5': 'Green',
+        '0.51': 'Yellow',
+        '0.75': 'Yellow',
+        '0.76': 'Red',
+        '1': 'Red'
+      },}).addTo(map);
+
+
+}
+
+addHeatMap();
