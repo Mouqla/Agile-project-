5503cfa69771522be89e6b486d6776e8a0bea8c6
themaxlevin <16058149+themaxlevin@users.noreply.github.com>
2024-04-19 09:58:07 +0200

    Only open 1 frame at a time

--- static/settings.js
+++ static/settings.js
@@ -1,3 +1,6 @@
+let frameList = [];
+var compareMode = false; //Ska tas bort senare
+
 let map = L.map('map').setView([57.7, 11.972], 12.5);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -14,70 +17,55 @@ async function onMapClick(e) {
     const response = await fetch(`/api/get_air?lat=${lat.toFixed(3)}&lng=${lng.toFixed(3)}&radius=${radius}`);
 
     const retJson = await response.json(); 
-    //console.log(retJson);
+
+    console.log(retJson);
+
+    const locationData = "";
 
     try {
         var apiResult = Object.values(retJson)[1][0];
-        //console.log(apiResult);
     
-        // Creates an Object that holds all the information for a single location
         function LocationData(apiResult){
             this.city = apiResult.city;
             this.location = apiResult.location;
-            
-            //formats the time last updated
-            var timeStr = apiResult.measurements[0].lastUpdated.toString();
-            var strLen = timeStr.length;
-            var date = timeStr.substring(0,10);
-            var hour = timeStr.substring(strLen-14,strLen-12);
-            var min = timeStr.substring(strLen-5,strLen-3);
-            var sec = timeStr.substring(strLen-2,strLen);
-            this.time = `${date} ${hour}:${min}:${sec}`;
-            
             this.closestLatitude = apiResult.coordinates.latitude;
             this.closestLongitude = apiResult.coordinates.longitude;
-            
-            //creates a map of the measured values
-            var pollutionMap = new Map();
+            this.pollution = new Pollution(apiResult);
+        }
+    
+        function Pollution(apiResult){
+            const pollutionMap = new Map();
             var measurements = apiResult.measurements;
             for(let i = 0; i < measurements.length; i++){
                 parameter = measurements[i].parameter;
-                //some sources supply temperature, humidity etc. filter out here?
                 value = measurements[i].value;
                 unit = measurements[i].unit;
-                lastUpdated = measurements[i].lastUpdated;
-                pollutionMap.set(parameter, [value, unit, lastUpdated]);
+                pollutionMap.set(parameter, [value, unit]);
             }
-            pollutionMap = new Map(Array.from(pollutionMap).sort()); //sort the map alphabetically
-
             this.pollution = pollutionMap;
         }
     
         const locationData = new LocationData(apiResult);
-
-        /* Lägger resultatet i en ny "frame" i sidebar*/
-        createAndAppendFrame(locationData);
+        console.log(locationData);
     } catch (error) {
         console.log(error);
         if (error instanceof TypeError) {
-            var popup = L.popup()
-            .setLatLng(e.latlng)
-            .setContent(`No results within ${radius} meters`)
-            .openOn(map);
+            console.log(`No results within ${radius} meters`);
         }
         else {
-            var popup = L.popup()
-            .setLatLng(e.latlng)
-            .setContent(`Unknown error fetching API data`)
-            .openOn(map);
+            console.log(`Unknown error fetching API data`);
         }
     }
+    
+    prepareNextFrame(compareMode);
+    createAndAppendFrame(locationData) /* Lägger resultatet i en ny "frame" i sidebar*/
+
 }
 
 
 /* Öppnar sidebaren (initialt utanför skärmen) */ /* TODO */
 function openNav() {
-    document.getElementById("offcanvas").style.width = "380";
+    document.getElementById("offcanvas").style.width = "382";
     document.getElementById("main").style.marginRight = "-200px";
 }
 
@@ -102,33 +90,40 @@ function createAndAppendFrame(content) {
             newFrame.innerHTML = html;
 
             const infoBox = newFrame.querySelector('#infoBox');
+            infoBox.innerHTML = content;
 
             const sidebar = document.getElementById('offcanvas');
             sidebar.appendChild(newFrame);
 
-            // TODO - closebutton borta?
             const closeButton = newFrame.querySelector('.closebtn');
             closeButton.addEventListener('click', function() {
                     closeFrame(newFrame.id);
                 });
 
-            //Lägger till mätvärden för luftföroreningar i sidebar 
-            var pollutionMap = content.pollution;
-            var mapIterator = pollutionMap[Symbol.iterator]();
-            for (const item of mapIterator) {
-                infoBox.innerHTML += item[0].toString() + ' ' // parameter aka type of pollution
-                infoBox.innerHTML += item[1][0]; //value
-                infoBox.innerHTML += item[1][1]; //unit
-                infoBox.innerHTML += '<br>'; //linebreak
-              }
+            document.getElementById("location").innerHTML = content.city;
+            
+            frameList.push(newFrame.id)
 
-            //Header med stad, mätstation och tid
-            var headerBox = newFrame.querySelector('#header');
-            headerBox.innerHTML = `<h1>${content.city}</h1>`;
-            headerBox.innerHTML += `<h2>${content.location}</h2>`
-            headerBox.innerHTML += `<h3>Last updated ${content.time}</h3>`;            
         })
+        
 }
+
+/* Stänger tidigare frame inför skapandet av nästa frame, med undantag för om användaren klickat i Compare Mode */
+function prepareNextFrame(compareMode) {
+    if (!compareMode) {
+        if (frameList.length > 0) {
+            closeFrame(frameList[0]);
+            frameList = [];
+        }
+        return;
+    }
+    if (frameList.length != 1) {
+        closeFrame(frameList[0]);
+        frameList = [];
+        return;
+    }
+}
+
 
 map.on('click', onMapClick);
 
