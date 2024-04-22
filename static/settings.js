f002ec6773a174c5f2ce6960f53a34edc5a1d5ac
Elias Nilsson <elinil0221@gmail.com>
2024-04-22 13:41:41 +0200

    heatmap function

--- static/settings.js
+++ static/settings.js
@@ -1,4 +1,5 @@
 let frameList = [];
+let heatMap;
 var compareMode = false; //Ska tas bort senare
 
 let map = L.map('map').setView([57.7, 11.972], 12.5);
@@ -312,4 +313,54 @@ function createSearchDropdown(locations){
     for(location in locations){
         searchDropdown.innerHTML += `<div>Hello!</div>`;
     }
+}
+
+async function fetchAirQualityData() {
+    const dataSet = [];
+    for (let i = 1; i< 19; i++){
+        if (i == 17){
+            continue
+        }else{
+            const url = `/api/get_points?page=${i}`;
+            const response = await fetch(url);
+            const data = await response.json();
+            dataSet.push(...data.results);
+        }
+    }
+    return dataSet;
+}
+
+
+
+async function addHeatMap(type){
+    const data = await fetchAirQualityData();
+    const heatPoints =[];
+    
+    data.forEach(item => {
+        if (item.coordinates && item.measurements) {
+            for (const measurement of item.measurements) {
+                if (measurement.parameter == type){
+                    heatPoints.push([
+                        item.coordinates.latitude,
+                        item.coordinates.longitude,
+                        measurement.value
+                    ]);
+                }
+            }
+        }
+    });
+
+    if (heatMap) {
+        map.removeLayer(heat);
+    }
+
+    heatMap = L.heatLayer(heatPoints, {max:3, blur:0, radius: 15, gradient: {
+        '0': 'Navy', '0.25': 'Navy',
+        '0.26': 'Green',
+        '0.5': 'Green',
+        '0.51': 'Yellow',
+        '0.75': 'Yellow',
+        '0.76': 'Red',
+        '1': 'Red'
+      },}).addTo(map);
 }
