a3f8f2003a35650d17e0cbb10310b07ef116aec9
bllnr <52850477+bllnr@users.noreply.github.com>
2024-05-14 16:34:15 +0200

    Add test

--- static/index.test.js
+++ static/index.test.js
@@ -1,4 +1,8 @@
-({getFormattedPollutionName, getQualitativeValue} = require('./location-data.js'))
+({getFormattedPollutionName, 
+  getQualitativeValue,
+  formatTimeFromUnix,
+  geocode
+} = require('./location-data.js'))
 
 test(`getFormattedPollutionName('banana'): Expects null`, () => {
   expect(getFormattedPollutionName('banana')).toBe(null);
@@ -11,3 +15,14 @@ test(`getQualitativeValue(1)[0]: Expects Good`, () => {
 test(`getQualitativeValue(55)[0]: Expects Unknown`, () => {
     expect(getQualitativeValue('55')[0]).toStrictEqual('Unknown');
   });
+
+test(`formatTimeFromUnix(0): Expects 1970-01-01 01:00:00`, () => {
+  expect(formatTimeFromUnix(0)).toStrictEqual('1970-01-01 01:00:00');
+})
+
+/*
+test(`geocode(Gothenburg, SE): Expects [57.7072326, 11.9670171 ]`, () => {
+  expect(geocode('Gothenburg, SE').toStrictEqual[57.7072326, 11.9670171 ])
+
+})
+*/
